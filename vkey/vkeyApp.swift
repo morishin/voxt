dd4d3f3c4d6379b11fe71bc09d2aa575b94d57a4
Shintaro Morikawa <sntr92@gmail.com>
2026-06-08 22:13:58 +0900

    Initial Commit

---
+++ vkey/vkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  vkeyApp.swift
+//  vkey
+//
+//  Created by morishin on 2026/06/08.
+//
+
+import SwiftUI
+
+@main
+struct vkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
